4a1bcb81c606da55da454d51643bd58face4d6e2
kiran <kiran.bhingare07@gmail.com>
2022-07-16 17:12:29 +0530

    third commit

--- src/sample/java.java
+++ src/sample/java.java
@@ -3,7 +3,8 @@ package sample;
 public class java {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("kiran");
+		System.out.println("kiran");
 
 	}
 
